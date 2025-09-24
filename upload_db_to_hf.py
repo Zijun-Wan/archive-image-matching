1c8305d4c0968a0581637af7caef591859005b1b
Zijun-Wan <nujiznaw8240@gmail.com>
2025-09-24 23:52:29 +0200

    uploading database to huggingface

---
+++ upload_db_to_hf.py
@@ -0,0 +1,15 @@
+from huggingface_hub import create_repo, upload_folder
+
+user_name = "nujiznaw"
+dataset_name = "vocab_db"
+repo_id = f"{user_name}/{dataset_name}"
+create_repo(repo_id, repo_type="dataset", private=False, exist_ok=True)
+
+upload_folder(
+    repo_id=repo_id,
+    repo_type="dataset",
+    folder_path="vocab_db_without_filtering",  # local folder with the 3 .pkl.gz files
+    path_in_repo="vocab_db_without_filtering", # optional subdir name on the Hub
+)
+
+print("Pushed to:", repo_id)
